c8c52ea377ec70c04b2b1b3155bd2239de646633
arsgpo <ars.gpo@protonmail.com>
2021-08-07 21:30:02 -0700

    added javascript code to project

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function computerPlay(){
+    const compSelection = ["rock", "paper", "scissors"];
+    let selection = compSelection[Math.floor(Math.random()*(compSelection.length))];
+    return selection;
+  }
+  
+  function playerPlay(){
+  const choice = prompt("pick rock, paper, or scissors ");
+  return choice;
+  }
+  
+  function playRound(playerSelection, computerSelection){
+  if ((computerSelection === playerSelection)){
+    alert("tie,try again!");
+  } else if ((computerSelection === "rock") && (playerSelection === "paper")){
+  alert("you win paper beats rock");
+  } else if ((computerSelection === "rock") && (playerSelection === "scissors")){
+  alert("you lose rock beats beats scissors");
+  } else if ((computerSelection === "scissors") && (playerSelection === "rock")){
+  alert("you win rock beats scissors");
+  } else if ((computerSelection === "scissors") && (playerSelection === "paper")){
+  alert("you lose scissors beats paper");
+  } else if ((computerSelection === "paper") && (playerSelection === "scissors")){
+  alert("you win paper beats scissors");
+  } else if ((computerSelection === "paper") && (playerSelection === "rock")){
+  alert("you lose paper beats rock");
+  }else alert("please pick rock paper or scissors");
+  }
+  const playerSelection = playerPlay();
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+  console.log(computerSelection);
+  console.log(playerSelection)
+  
